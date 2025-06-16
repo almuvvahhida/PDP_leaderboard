e299c76debbecd8f83351ddd51e6ea69dfb19e36
dilshod-py <absaitovdev@gmail>
2025-06-16 18:01:47 +0500

    first commit

--- root/settings.py
+++ root/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'drf_spectacular_sidecar',
     # JWT
     'rest_framework_simplejwt',
-
 ]
 
 MIDDLEWARE = [
@@ -55,6 +54,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'root.urls'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
