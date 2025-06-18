9fb51be3800d4c74842326d8fd15e4a710304865
almuvvahhida <muvvahhida@gmail.com>
2025-06-18 16:28:25 +0500

    new

--- admin/urls.py
+++ admin/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 from admin.views import SessionViewSet, TeacherViewSet
 
+router = DefaultRouter()
+router.register(r'teacher', TeacherViewSet, basename='teacher')
+router.register(r'session', SessionViewSet, basename='session')
+
 urlpatterns = [
-    path('teacher', TeacherViewSet.as_view()),
-    path('session', SessionViewSet.as_view()),
+    path('', include(router.urls)),
 ]
